e74467ec46fad1b84d02de886e7574d458c69919
NachoMol <ignaciotmolina@gmail.com>
2025-06-03 20:23:36 -0300

    switchs

--- gopath/conditionals/main.go
+++ gopath/conditionals/main.go
@@ -5,17 +5,36 @@ import (
 )
 
 func main() {
-	yearsOld := 33
 
-	if yearsOld > 18 {
-		fmt.Printf("You are an adult, you are %d years old\n", yearsOld)
-	} else {
-		fmt.Printf("You are a minor, you are %d years old\n", yearsOld)
+	/*
+
+		yearsOld := 33
+
+		if yearsOld > 18 {
+			fmt.Printf("You are an adult, you are %d years old\n", yearsOld)
+		} else {
+			fmt.Printf("You are a minor, you are %d years old\n", yearsOld)
+		}
+
+		if value := true; value {
+			fmt.Println("This is true")
+		} else {
+			fmt.Println("This is false")
+		}
+	*/
+
+	number := 5
+
+	switch number {
+	case 1:
+		fmt.Println("Number is one")
+	case 2:
+		fmt.Println("Number is two")
+	case 3:
+		fmt.Println("Number is three")
+	default:
+		fmt.Println("Number is something else")
+
 	}
 
-	if value := true; value {
-		fmt.Println("This is true")
-	} else {
-		fmt.Println("This is false")
-	}
 }
